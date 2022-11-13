11bb2e9ca197e86da254648eacdb3075681feee7
juwon <juwon@Akanz-mac.local>
2022-11-12 21:28:57 -0600

    update

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -15,9 +15,7 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
+           The best way to keep track of all your job applications. 
           </p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
